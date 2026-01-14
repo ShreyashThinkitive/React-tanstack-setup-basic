dd9e2ff37cd3c0d05e55ffa32a0f05812f2fe172
Shreyash Chougule <shreyash.chougule@thinkitive.com>
2026-01-14 20:34:48 +0530

    using tanstack

--- Tanstak-setup/src/App.tsx
+++ Tanstak-setup/src/App.tsx
@@ -1,33 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const fetchPosts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  return res.json()
+}
+
+const addPost = async (post) => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(post),
+  })
+}
+
+
+
+function App() {
+  const queryClient = useQueryClient()
+
+  const { data, isPending } = useQuery<any>({
+    queryKey: ['posts'],
+    queryFn: fetchPosts
+  })
+
+
+  const { mutate } = useMutation({
+    mutationFn: addPost,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['posts']
+      })
+    }
+  })
+
+
+
+  if (isPending) return (
+    <div>
+      ...Loding
+    </div>
+  )
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ padding: '20px' }}>
+        <h2>Posts Table</h2>
+        <button onClick={() => mutate({
+          userId: 101,
+          id: 101,
+          title: "this is my post",
+          body: "hello my name is shreyas"
+        })}>Add Post</button>
+        <br /><br />
+
+        <table border={1} cellPadding={10} cellSpacing={0}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>User ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {data?.map(post => (
+              <tr key={post.id}>
+                <td>{post.id}</td>
+                <td>{post.userId}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
